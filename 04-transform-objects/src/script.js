4a5272f9fd2af6bc0ecc61c20813c096c8e10f78
vendredl <node.js_@kakao.com>
2024-04-05 17:12:32 +0900

    :speech_balloon: Start Transform Objects

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -7,7 +7,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Object
+ * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
